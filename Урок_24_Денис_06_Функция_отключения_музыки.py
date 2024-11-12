c11a9281db4ad92f474e688311385bdd6b3688c2
Programmer <shakirov.ekb@gmail.com>
2024-11-13 01:25:37 +0500

    Увеличен шрифт и убраны секунды.

---
+++ Урок_24_Денис_06_Функция_отключения_музыки.py
@@ -0,0 +1,56 @@
+from tkinter import *
+from tkinter import messagebox as mb
+from tkinter import simpledialog as sd
+import datetime
+import time
+import pygame
+
+
+t = 0
+
+
+def set():
+    global t
+    rem = sd.askstring("Время напоминания","Введите время напоминания в формате ЧЧ:ММ в 24 сасовом формате)")
+    if rem:
+        try:
+            hour = int(rem.split(":")[0])
+            minute = int(rem.split(":")[1])
+            now = datetime.datetime.now()
+            print(now)
+            dt = now.replace(hour=hour, minute=minute, second=0)
+            print(dt)
+            t = dt.timestamp()
+            print(t)
+        except Exception as e:
+            mb.showerror("Ошибка!", f"Произошла ошибка {e}")
+            print(e)
+
+
+def check():
+    global t
+    if t:
+        now = time.time()
+        if now >= t:
+            play_snd()
+            t = 0
+    window.after(10000, check)
+
+
+def play_snd():
+    pygame.mixer.init()
+    pygame.mixer.music.load("reminder.mp3")
+    pygame.mixer.music.play()
+
+window = Tk()
+window.title("Напоминание")
+
+label = Label(text="Установите напоминание", font=("Arial", 14))
+label.pack(pady=10)
+
+set_button = Button(text="Установить напоминание", command=set)
+set_button.pack(pady=10)
+
+check()
+
+window.mainloop()
